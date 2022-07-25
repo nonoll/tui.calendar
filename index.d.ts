eab55b6d8334bff8672a63042d2603bca1804228
nonoll <hyunkwan.roh@nhnsoft.com>
2022-07-25 18:10:21 +0900

    fix: bundle versioning

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TOAST UI Calendar v1.15.3 for multiple instance of calendar
+// Type definitions for TOAST UI Calendar v1.15.3
 // TypeScript Version: 3.2.1
 
 export type DateType = string | Date | TZDate;
